960939acdf456b38bf66af02e35dd5a52103b871
Jeffrey Dunham <jeffrey.a.dunham@gmail.com>
2015-11-12 01:24:56 -0500

    Adding in paginated calls

--- abiosgaming/base_client.py
+++ abiosgaming/base_client.py
@@ -27,27 +27,45 @@ class BaseAbiosClient(object):
     def access_token(self):
         return "u8vhWfs07Y7PuhdSJAqyPYKbDAHsZdNKW6Df9WYJ";
 
+    @property
+    def next_page(self):
+        try:
+            return self._next_page
+        except AttributeError:
+            pass
+
+    @next_page.setter
+    def next_page(self, value):
+        logging.debug("Setting value of next_page to {}".format(value))
+        self._next_page = value
+
+    @next_page.deleter
+    def next_page(self):
+        try:
+            del self._next_page
+        except AttributeError:
+            pass
+
     def _build_url(self, path):
         return '/'.join([self._endpoint] + path)
 
-    def _call(self, url, response_type='iter', **parameters):
-        response_types = ('iter', 'json')
-        assert response_type in response_types
+    def _call(self, url, **parameters):
+        del self.next_page
 
         # Get our response using the session
         try:
             response = self.session.get(url,
                                         params=parameters,
-                                        verify=True,
-                                        stream=True)
+                                        verify=True)
         except MaxRetryError as e:
             logging.exception(e)
             raise e
 
-        logging.debug(response)
-        logging.debug(response.headers['link'])
-        logging.debug(response.links["next"])
-        logging.debug(response.links["last"])
+        if(response.links["next"]):
+            logging.debug("adding next_page: {}".format(response.links["next"]))
+            self.next_page = response.links["next"]["url"]
+
+        logging.debug(response.links["next"]["url"])
         try:
             response.raise_for_status()  # this will raise on 4xx and 5xxs
         except HTTPError as e:
@@ -55,10 +73,30 @@ class BaseAbiosClient(object):
             raise e
 
         data = response.json()
+        logging.debug(data)
         return data
 
-    def _get_matches(self, parameters):
+    def _get_next_page(self):
+        logging.debug("Am I getting here?")
+        if(self.next_page):
+            logging.debug("Calling the next URL: {}".format(self.next_page))
+            return self._call(self.next_page)
+        return None
+
+    def _paginated_call(self, url, item_count=3, **parameters):
+        logging.debug("I'm looking for {} items".format(item_count))
+        data = self._call(url, **parameters)
+        assert isinstance(data, list)
+        logging.debug(len(data))
+        while( len(data) < item_count ):
+            logging.debug("Calling next page")
+            data.extend(self._get_next_page())
+            logging.debug("Data size is now {}".format(len(data)))
+
+        return data[:item_count]
+
+    def _get_matches(self, parameters, count=3):
         path = ['v1', 'matches']
         url = self._build_url(path)
         parameters['access_token'] = self.access_token
-        return self._call(url, **parameters)
+        return self._paginated_call(url, item_count=count, **parameters)
--- abiosgaming/client.py
+++ abiosgaming/client.py
@@ -4,14 +4,15 @@ from base_client import BaseAbiosClient
 
 class AbiosClient(BaseAbiosClient):
     def get_upcoming_matches(self,
+                             count=3,
                              addons=[],
                              games=[],
                              sort='ASC'
                             ):
-        return self._get_matches({"with[]": addons, "games[]": games, "starts_after": "now", "sort": sort})
+        return self._get_matches({"with[]": addons, "games[]": games, "starts_after": "now", "sort": sort}, count=count)
 
 if __name__ == "__main__":
     abios = AbiosClient()
-    matches = abios.get_upcoming_matches(addons=['tournament', 'matchups'], games='5')
+    matches = abios.get_upcoming_matches(count=18, addons=['tournament', 'matchups'], games='5')
     print matches[0]
 
