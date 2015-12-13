ba9f02879a5f00f02e9db997619fd7e6b6898520
Jeffrey Dunham <jeffrey.a.dunham@gmail.com>
2015-12-12 19:08:21 -0500

    Adding in new functionality to match object

--- abiosgaming/match.py
+++ abiosgaming/match.py
@@ -1,5 +1,7 @@
 import json
 from .exceptions import NoMatchupFound
+from collections import OrderedDict
+
 
 class Match:
     """
@@ -34,4 +36,34 @@ class Match:
             return self._raw_data['matchups']
         except KeyError:
             raise NoMatchupFound
+
+    def get_competitors_name_list(self):
+        return [competitor['name']
+         for competitor in matchup['competitors']
+         for matchup in self.matchups[0]]
+
+    def get_competitor_list(self):
+        return self.matchups[0]['competitors']
+
+    def get_score(self):
+        score_map = OrderedDict()
+        scores = self._get_raw_score()
+        for id, score in scores.items():
+            score_map[ self.find_competitor_by_id(id)['name'] ] = score
+
+        return score_map
+
+    def _get_raw_score(self):
+        return self.matchups[0]['scores']
+
+    def find_competitor_by_id(self, competitor_id):
+        competitor_id = int(competitor_id)
+        competitors = self.get_competitor_list()
+        for competitor in competitors:
+            if competitor['id'] == competitor_id:
+                return competitor
+        raise "Could not find competitor"
+
+
+
 
